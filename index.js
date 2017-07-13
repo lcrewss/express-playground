e2cab434529a1ef133141b458dd4021a1a29e403
Logan Crewss <logancrewss@Blakes-MacBook-Air.local>
2017-07-13 15:02:58 -0600

    Calculate with Querystring

--- index.js
+++ index.js
@@ -6,5 +6,29 @@ app.get("/math/pi", function(request,response){
   response.send(String(Math.PI))
 })
 
+// Calulate with Querystrings
+
+
+app.get("/math/calculate", function(request, response){
+  var operation = request.query.operation
+  var x = parseInt(request.query.x)
+  var y = parseInt(request.query.y)
+  var result = 0
+  var string
+  if(operation === "add"){
+    result = x + y
+    string = `${x} + ${y} = ${result}`
+  }else if (operation === "subtract"){
+    result = x - y
+    string = `${x} - ${y} = ${result}`
+  }else if (operation === "multiply"){
+    result = x * y
+    string = `${x} * ${y} = ${result}`
+  }else if (operation === "division"){
+    result = x / y
+    string = `${x} / ${y} = ${result}`
+  }
+  response.send(string);
+})
 
 app.listen(port)
